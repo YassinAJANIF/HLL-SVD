1ee3c80a22f08c43e08f8cf87d2c79ad34b21f7f
Yassin Ajanif <yajanif@ec-nantes.fr>
2025-05-15 18:10:27 +0200

    improve setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 from setuptools import setup, find_packages
 
-setup(
-    name='Name',
-    version='0.1.0',
-    author='',
-    author_email='',
-    description='',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='',  # ici il faut mettre le lien  GitHub
-    packages=find_packages(),
-    install_requires=[
-        'numpy',
-        'scipy',
-        'mpi4py',
-        'cupy',
-        'h5py',
-    ],
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.8',
-)
+# Charger les dépendances depuis un fichier requirements.txt
+def load_requirements(filename):
+    with open(filename) as f:
+        return f.read().splitlines()
 
+setup(
+    name="nom_du_projet",
+    version="1.0.0",
+    author="Votre Nom",
+    author_email="votre_email@example.com",
+    description="Description de votre projet",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/votre_nom/votre_projet",
+    packages=find_packages(),
+    install_requires=load_requirements("requirements.txt"),  # Charge les dépendances
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",  # Version minimale de Python
+)
