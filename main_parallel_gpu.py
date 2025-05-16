28fbe404690a567827b2197b8610d013685caa38
llestand <lucas.lestandi@ec-nantes.f>
2025-05-16 11:40:24 +0200

    fixing paths

--- main_parallel_gpu.py
+++ main_parallel_gpu.py
@@ -11,29 +11,21 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'Paralle
 #from Parallel_svd.base_parallel import ParSVD_Base
 from parallel_svd.stream_svd_gpus import ParSVD_Parallel
 
-
-
 #Path to the data
-path = "/home/yaajanif/Mylibrary/Data/Data_parallel/"
+path = "Data/Data_parallel/"
 
 # Creation of the ParSVD_Parallel instance with K=10 and forget factor ff=10.
 parallel = ParSVD_Parallel(K=10, ff=10)
 
-
-
 #Load the data
 data0 = np.load(os.path.join(path, f'points_rank_{parallel.rank}_batch_0.npy')).astype(np.float64)
 data1=  np.load(os.path.join(path, f'points_rank_{parallel.rank}_batch_1.npy')).astype(np.float64)
-
-
-
 
 # Initialisation et incorporation des données.
 t1=MPI.Wtime()
 parallel.initialize(data0)
 parallel.incorporate_data(data1)
 t2=MPI.Wtime()
-
 
 # start a second pass over the data to compute Vt
 t3=MPI.Wtime()
