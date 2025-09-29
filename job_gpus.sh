d0da737b56b5f6af680506f64bae520cae8a7d33
Yassin Ajanif <yajanif@ec-nantes.fr>
2025-09-29 17:05:38 +0200

    update script slurm

--- job_gpus.sh
+++ job_gpus.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=gpu
-#SBATCH --cluster=nautilus
-#SBATCH --partition=gpu            # Specify the partition
-#SBATCH --nodelist=gnode1          # Specify the node name
+#SBATCH --nodes=1                  #Totla number of nodes
+#SBATCH --gres=gpu:4               #Number of gpus per node
+#SBATCH --constraint=h100          #use of gpus H100
 #SBATCH --ntasks=4                 # Total number of task
-#SBATCH --gres=gpu:4               # Number of gpus per node
-#SBATCH --qos=gpus
-#SBATCH --time=00:40:00
+#SBATCH --cpus-per-task=1             
+#SBATCH --time=01:30:00
 #SBATCH --output=output_gpu/test.log
 #SBATCH --error=output_gpu/test.err
 #SBATCH --exclusive                   
@@ -15,13 +14,13 @@
 source ~/.bashrc
 #source activate new_env
 
-conda activate new_env
+conda activate new_env                    # Change the env name
 export OPENBLAS_NUM_THREADS=1
 export MKL_NUM_THREADS=1
 export OMP_NUM_THREADS=1
 
 
 
-# Execution with 2 GPUs
+# Execution with 4 GPUs
 
-mpirun -np 2  python3  main_parallel_gpu.py
+mpirun -np 4  python3  main_parallel_gpu.py
