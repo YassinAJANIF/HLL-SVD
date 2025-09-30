bfd5837f1430a845926179a02494aa18568880cf
Yassin Ajanif <yajanif@ec-nantes.fr>
2025-09-30 12:18:48 +0200

    update slurm script

--- job_gpus.sh
+++ job_gpus.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 #SBATCH --job-name=gpu
-#SBATCH --nodes=1                  #Totla number of nodes
-#SBATCH --gres=gpu:4               #Number of gpus per node
-#SBATCH --constraint=h100          #use of gpus H100
-#SBATCH --ntasks=4                 # Total number of task
-#SBATCH --cpus-per-task=1             
+#SBATCH --nodes=1                  # Total number of nodes
+#SBATCH --gres=gpu:4               # GPUs per node
+#SBATCH --constraint=h100          # Request H100 GPUs
+#SBATCH --ntasks=4                 # Total number of MPI tasks
+#SBATCH --cpus-per-task=1
 #SBATCH --time=01:30:00
 #SBATCH --output=output_gpu/test.log
 #SBATCH --error=output_gpu/test.err
-#SBATCH --exclusive                   
+#SBATCH --exclusive                # Use the node exclusively
 
-# Chargement de l'environnement
+# Load environment
 source ~/.bashrc
-#source activate new_env
+# source activate new_env
 
-conda activate new_env                    # Change the env name
+conda activate new_env             # Change the env name if needed
 export OPENBLAS_NUM_THREADS=1
 export MKL_NUM_THREADS=1
 export OMP_NUM_THREADS=1
 
+# Run with 4 GPUs (1 MPI rank per GPU)
+mpirun -np 4 python3 main_parallel_gpu.py
 
-
-# Execution with 4 GPUs
-
-mpirun -np 4  python3  main_parallel_gpu.py
